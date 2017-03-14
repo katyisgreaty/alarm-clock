54cbc8fac1eb0c5a5aebe2d21150506260c71221
Elise St Hilaire and Katy Daviscourt <elise+katy@>
2017-03-14 16:28:53 -0700

    add fail option to promise

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -45,8 +45,11 @@ $(document).ready(function() {
     var city = $("#location").val();
     $("#location").val("");
     $(".showCity").text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey) .then(function(response) {
       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    });
+    })
+    .fail(function(error) {
+        $('.showWeather').text(error.responseJSON.message);
+      });
   });
 });
