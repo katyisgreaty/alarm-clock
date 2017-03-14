e1e30689bad180a932bdec534ad2a9575b2d9081
Elise St Hilaire and Katy Daviscourt <elise+katy@>
2017-03-14 15:46:03 -0700

    add basic weather functionality

--- build/js/app.js
+++ build/js/app.js
@@ -69,4 +69,17 @@ $(document).ready(function(){
   });
 });
 
+var apiKey = "433e8c7286499246ed8f97e2764cdf79";
+
+$(document).ready(function() {
+  $("#weatherLocation").click(function(){
+    var city = $("#location").val();
+    $("#location").val("");
+    $(".showWeather").text("The city you have chosen is " + city + ".");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
+
 },{"./../js/alarm.js":1}]},{},[2]);
