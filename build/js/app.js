fcc58569091bb9c8a413fc9646a84cf8f9f1f067
Elise St Hilaire and Katy Daviscourt <elise+katy@>
2017-03-14 15:48:00 -0700

    change output location slightly

--- build/js/app.js
+++ build/js/app.js
@@ -75,7 +75,7 @@ $(document).ready(function() {
   $("#weatherLocation").click(function(){
     var city = $("#location").val();
     $("#location").val("");
-    $(".showWeather").text("The city you have chosen is " + city + ".");
+    $(".showCity").text("The city you have chosen is " + city + ".");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
       $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
     });
